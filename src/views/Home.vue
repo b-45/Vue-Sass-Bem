64801b5b363d140016a5b2451adca821bcca0e1e
b-45 <bernard11ade@gmail.com>
2018-05-29 21:44:20 -0400

    add container

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,11 +1,10 @@
 <template>
-  <div class="home">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
-  </div>
+ <div class="container">
+
+ </div>
 </template>
 
 <script>
-// @ is an alias to /src
 import HelloWorld from '@/components/HelloWorld.vue'
 
 export default {
@@ -15,3 +14,13 @@ export default {
   }
 }
 </script>
+
+<style lang="scss" scoped>
+.container {
+  max-width: 120rem; // width for this container (1200px) 
+  margin: 8rem auto; // centers container
+  background-color: var(--color-grey-light-2);
+  box-shadow: var(--shadow-dark);
+  min-height: 50rem;
+}
+</style>
