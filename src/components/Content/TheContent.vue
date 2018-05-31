66461596a5f62746c72844d140c4a70e5b7f2dbd
b-45 <bernard11ade@gmail.com>
2018-05-30 20:45:24 -0400

    created TheSidebar & TheHotelView component

--- src/components/Content/TheContent.vue
+++ src/components/Content/TheContent.vue
@@ -1,5 +1,29 @@
 <template>
-  <div>
-    content
+  <div class="content"> 
+    <TheSidebar></TheSidebar>
+    <TheHotelView></TheHotelView>
   </div>
 </template>
+
+<script>
+import TheSidebar from '@/components/Content/TheSidebar/TheSidebar.vue'
+import TheHotelView from '@/components/Content/TheHotelView/TheHotelView.vue'
+
+
+export default {
+  components: {
+    TheSidebar,
+    TheHotelView
+  }
+  
+}
+</script>
+
+<style lang="scss" scoped>
+  .content{
+    display: flex; // places side bar and content side by side. 
+  }
+
+
+</style>
+
