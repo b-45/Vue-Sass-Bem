4c2e3acba09149889b52ce622cd88239f8e87df2
b-45 <bernard11ade@gmail.com>
2018-06-01 12:31:58 -0400

    added PhotoGallery component

---
+++ src/components/Content/TheHotelView/PhotoGallery.vue
@@ -0,0 +1,23 @@
+<template>
+  <div class="gallery">
+      <figure class="gallery__item">
+        <img src="@/img/hotel-1.jpg" alt="" class="gallery__photo">
+      </figure>
+      <figure class="gallery__item">
+        <img src="@/img/hotel-2.jpg" alt="" class="gallery__photo">
+      </figure>
+      <figure class="gallery__item">
+        <img src="@/img/hotel-3.jpg" alt="" class="gallery__photo">
+      </figure>
+    </div>
+</template>
+
+<style lang="scss" scoped>
+  .gallery {
+  display: flex;
+  &__photo {
+    width: 100%;
+    display: block;
+  }
+  }
+</style>
--- src/components/Content/TheHotelView/TheHotelView.vue
+++ src/components/Content/TheHotelView/TheHotelView.vue
@@ -1,3 +1,26 @@
 <template>
-  
+  <div class="hotel-view">
+    <PhotoGallery></PhotoGallery>
+  </div>
 </template>
+
+<script>
+import PhotoGallery from '@/components/Content/TheHotelView/PhotoGallery.vue';
+
+export default {
+  components: {
+    PhotoGallery,
+    
+    }
+}
+</script>
+
+
+<style lang="scss" scoped>
+  .hotel-view {
+    background-color: #fff;
+    flex: 1; // flex 1 grows the remaining width next to sidebar
+  }
+</style>
+
+
