95105506e5cb71c9e2fc80703757357d9547d191
b-45 <bernard11ade@gmail.com>
2018-06-02 20:43:45 -0400

    added a html template to HotelOverview

---
+++ src/components/Content/TheHotelView/HotelOverview.vue
@@ -0,0 +1,40 @@
+<template>
+  <div class="overview">
+    <h1 class="overview__heading">
+      Hotel Las Palmas
+    </h1>
+    <div class="overview__stars">
+      <svg class="overview__icon-star" version="1.1" xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 20 20">
+        <title>star</title>
+        <path d="M10 1.3l2.388 6.722h6.412l-5.232 3.948 1.871 6.928-5.439-4.154-5.438 4.154 1.87-6.928-5.233-3.948h6.412l2.389-6.722z"></path>
+      </svg>
+      <svg class="overview__icon-star" version="1.1" xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 20 20">
+        <title>star</title>
+        <path d="M10 1.3l2.388 6.722h6.412l-5.232 3.948 1.871 6.928-5.439-4.154-5.438 4.154 1.87-6.928-5.233-3.948h6.412l2.389-6.722z"></path>
+      </svg>
+      <svg class="overview__icon-star" version="1.1" xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 20 20">
+        <title>star</title>
+        <path d="M10 1.3l2.388 6.722h6.412l-5.232 3.948 1.871 6.928-5.439-4.154-5.438 4.154 1.87-6.928-5.233-3.948h6.412l2.389-6.722z"></path>
+      </svg>
+      <svg class="overview__icon-star" version="1.1" xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 20 20">
+        <title>star</title>
+        <path d="M10 1.3l2.388 6.722h6.412l-5.232 3.948 1.871 6.928-5.439-4.154-5.438 4.154 1.87-6.928-5.233-3.948h6.412l2.389-6.722z"></path>
+      </svg>
+      <svg class="overview__icon-star" version="1.1" xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 20 20">
+        <title>star</title>
+        <path d="M10 1.3l2.388 6.722h6.412l-5.232 3.948 1.871 6.928-5.439-4.154-5.438 4.154 1.87-6.928-5.233-3.948h6.412l2.389-6.722z"></path>
+      </svg>
+    </div>
+    <div class="overview__location">
+      <svg class="overview__icon-location" version="1.1" xmlns="http://www.w3.org/2000/svg" width="20" height="20" viewBox="0 0 20 20">
+        <title>location-pin</title>
+        <path d="M10 2.009c-2.762 0-5 2.229-5 4.99 0 4.774 5 11 5 11s5-6.227 5-11c0-2.76-2.238-4.99-5-4.99zM10 9.76c-1.492 0-2.7-1.209-2.7-2.7s1.208-2.7 2.7-2.7c1.49 0 2.699 1.209 2.699 2.7s-1.209 2.7-2.699 2.7z"></path>
+      </svg>
+      <button class="btn-inline">Lagos, Nigeria</button>
+    </div>
+    <div class="overview__rating">
+      <div class="overview__rating-average">8.6</div>
+      <div class="overview__rating-count">429 votes</div>
+    </div>
+  </div>
+</template>
