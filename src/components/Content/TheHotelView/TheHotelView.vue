23da7397a9f7653d73471d3c966fc689950a0211
b-45 <bernard11ade@gmail.com>
2018-06-03 18:26:46 -0400

    added HotelDetail component

--- src/components/Content/TheHotelView/TheHotelView.vue
+++ src/components/Content/TheHotelView/TheHotelView.vue
@@ -2,6 +2,8 @@
   <div class="hotel-view">
     <PhotoGallery></PhotoGallery>
     <HotelOverview></HotelOverview>
+    <HotelDetail></HotelDetail>
+    
 
   </div>
 </template>
@@ -9,12 +11,14 @@
 <script>
 import PhotoGallery from '@/components/Content/TheHotelView/PhotoGallery.vue'
 import HotelOverview from '@/components/Content/TheHotelView/HotelOverview.vue'
+import HotelDetail from '@/components/Content/TheHotelView/HotelDetail.vue';
 
 
 export default {
   components: {
     PhotoGallery,
-    HotelOverview
+    HotelOverview,
+    HotelDetail
     
     }
 }
